b6f0057abf8dae301709f4db5ecbad81e1d84f61
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-26 19:23:34 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  attr_accessor :title, :author
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    if self.author == nil
+      return nil
+    else self.author.name
+    end
+  end
+end
