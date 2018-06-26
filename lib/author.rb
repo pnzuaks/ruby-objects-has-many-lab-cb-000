39b4a79f8dd0f43607f53f89abad2560f8c78cd1
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-26 18:58:49 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,30 @@
 class Author
 
-  def initialize()
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def self.post_count
+    @posts.count
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.artist = self
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
   end
 
+  
 end
